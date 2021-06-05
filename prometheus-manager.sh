5b8cb647b39b969042007f0fe85098024c3fc80f
Matthieu Borgognon <matrash@bqn.ch>
2021-06-05 10:24:19 +0200

    Refactor actions in methods + add global var for ports

--- prometheus-manager.sh
+++ prometheus-manager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-printf "Welcome to Prometheus manager!\n" # print to screen
+printf "Welcome to Prometheus manager!\n\n" # print to screen
 SYSTEM_ARCH=amd64 #arm64
 
 function usage {
@@ -18,9 +18,20 @@ function usage {
         exit 1
 }
 
+# Define list of arguments expected in the input
+# The following getopts command specifies that options N and P have arguments
+OPTSTRING=":uidnN:pP:ksvr:"
+
+if [[ ${#} -eq 0 ]]; then
+   usage
+fi
+
 # Set default values
 NODE_EXPORTER_VERSION=1.1.2
 PROMETHEUS_VERSION=2.27.1
+
+NODE_EXPORTER_PORT=9500
+PROMETHEUS_PORT=9590
 
 UPDATE_NODE_EXPORTER=false
 UPDATE_PROMETHEUS=false
@@ -31,12 +42,9 @@ INIT_PROMETHEUS=false
 NODE_TRIGGER=false
 PROMETHEUS_TRIGGER=false
 
-if [[ ${#} -eq 0 ]]; then
-   usage
-fi
 
 function get_versions() {
-  printf "\nInstallation version for node_exporter will be: %s" "$NODE_EXPORTER_VERSION"
+  printf "Installation version for node_exporter will be: %s" "$NODE_EXPORTER_VERSION"
 
   curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep "tag_name" |
   awk '{printf "\nHighest available version for node_exporter is: %s", substr($2, 3, length($2)-4)}'
@@ -53,11 +61,138 @@ function get_versions() {
   test -f /usr/local/bin/promtool && echo Promtool is present
 }
 
-# Define list of arguments expected in the input
-# The following getopts command specifies that options N and P have arguments
-optstring=":uidnN:pP:ksvr:"
 
-while getopts ${optstring} arg; do
+function get_status() {
+  systemctl status node_exporter | awk 'NR==3 {printf "Status of node_exporter: %s\n", $2}'
+  systemctl status prometheus | awk 'NR==3 {printf "Status of Prometheus: %s\n", $2}'
+}
+
+
+function set_users() {
+  useradd --no-create-home --shell /usr/sbin/nologin prometheus &> /dev/null || grep prometheus /etc/passwd
+  useradd --no-create-home --shell /bin/false node_exporter &> /dev/null || grep node_exporter /etc/passwd
+  echo
+}
+
+
+function update_node_exporter() {
+  printf "Update node_exporter\n"
+  test -f node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}.tar.gz || wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}.tar.gz
+
+  tar xfz node_exporter-*.tar.gz &> /dev/null
+  cp node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}/node_exporter /usr/local/bin
+  chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+  rm -rf node_exporter-${NODE_EXPORTER_VERSION}*
+}
+
+
+function init_node_exporter() {
+  cat > /etc/systemd/system/node_exporter.service <<EOM
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter \
+  --collector.systemd \
+  --collector.processes \
+  --web.listen-address=:${NODE_EXPORTER_PORT}
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+  systemctl daemon-reload
+  systemctl enable node_exporter
+  systemctl start node_exporter
+}
+
+
+function set_prometheus_folders() {
+  mkdir /etc/prometheus &> /dev/null
+  mkdir /var/lib/prometheus &> /dev/null
+  ls /etc | grep prometheus | awk '{printf "Directories %s:\n", $1}'
+  chown prometheus:prometheus /etc/prometheus &> /dev/nul4l && ls -all /etc | grep prometheus
+  chown prometheus:prometheus /var/lib/prometheus && ls -all /var/lib/ | grep prometheus
+  echo
+}
+
+
+function update_prometheus() {
+  printf "Update Prometheus\n"
+  test -f prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}.tar.gz || wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}.tar.gz
+
+  tar xfz prometheus-*.tar.gz &> /dev/null
+  cp prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/prometheus /usr/local/bin
+  cp prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/promtool /usr/local/bin
+  chown prometheus:prometheus /usr/local/bin/prometheus
+  chown prometheus:prometheus /usr/local/bin/promtool
+
+  cp -r prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/consoles /etc/prometheus
+  cp -r prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/console_libraries /etc/prometheus
+  chown -R prometheus:prometheus /etc/prometheus/consoles
+  chown -R prometheus:prometheus /etc/prometheus/console_libraries
+
+  rm -rf prometheus-${PROMETHEUS_VERSION}*
+}
+
+
+function init_prometheus() {
+  cat > /etc/prometheus/prometheus.yml <<EOM
+global:
+  scrape_interval:     15s
+  evaluation_interval: 15s
+
+rule_files:
+  # - "first.rules"
+  # - "second.rules"
+
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:${PROMETHEUS_PORT}']
+
+  - job_name: 'node_exporter'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:${NODE_EXPORTER_PORT}']
+EOM
+
+cat > /etc/systemd/system/prometheus.service <<EOM
+[Unit]
+  Description=Prometheus Monitoring
+  Wants=network-online.target
+  After=network-online.target
+
+[Service]
+  User=prometheus
+  Group=prometheus
+  Type=simple
+  ExecStart=/usr/local/bin/prometheus \
+  --config.file /etc/prometheus/prometheus.yml \
+  --storage.tsdb.path /var/lib/prometheus/ \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=:${PROMETHEUS_PORT}
+  ExecReload=/bin/kill -HUP ${MAINPID}
+
+[Install]
+  WantedBy=multi-user.target
+EOM
+
+  systemctl daemon-reload
+  systemctl enable prometheus
+  systemctl start prometheus
+}
+
+
+while getopts ${OPTSTRING} arg; do
   case "${arg}" in
     u)
       UPDATE_NODE_EXPORTER=true
@@ -88,12 +223,11 @@ while getopts ${optstring} arg; do
     k)
       systemctl stop node_exporter
       systemctl stop prometheus
+      get_status
       exit 1
       ;;
     s)
-      echo
-      systemctl status node_exporter | awk 'NR==3 {printf "Status of node_exporter: %s\n", $2}'
-      systemctl status prometheus | awk 'NR==3 {printf "Status of Prometheus: %s\n", $2}'
+      get_status
       exit 1
       ;;
     v)
@@ -101,7 +235,6 @@ while getopts ${optstring} arg; do
       exit 1
       ;;
     r)
-      echo
       systemctl stop node_exporter
       systemctl stop prometheus
       rm /usr/local/bin/node_exporter
@@ -124,119 +257,25 @@ while getopts ${optstring} arg; do
   esac
 done
 
-echo
-useradd --no-create-home --shell /usr/sbin/nologin prometheus &> /dev/null || grep prometheus /etc/passwd
-useradd --no-create-home --shell /bin/false node_exporter &> /dev/null || grep node_exporter /etc/passwd
 
-mkdir /etc/prometheus &> /dev/null
-mkdir /var/lib/prometheus &> /dev/null
-ls /etc | grep prometheus | awk '{printf "\nDirectories %s:\n", $1}'
-chown prometheus:prometheus /etc/prometheus &> /dev/nul4l && ls -all /etc | grep prometheus
-chown prometheus:prometheus /var/lib/prometheus && ls -all /var/lib/ | grep prometheus
+set_users
 
 if $UPDATE_NODE_EXPORTER && $NODE_TRIGGER; then
-  printf "\nUpdate node_exporter\n"
-  test -f node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}.tar.gz || wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}.tar.gz
-
-  tar xfz node_exporter-*.tar.gz &> /dev/null
-  cp node_exporter-${NODE_EXPORTER_VERSION}.linux-${SYSTEM_ARCH}/node_exporter /usr/local/bin
-  chown node_exporter:node_exporter /usr/local/bin/node_exporter
-
-  rm -rf node_exporter-${NODE_EXPORTER_VERSION}*
+  update_node_exporter
 fi
 
 if $INIT_NODE_EXPORTER && $NODE_TRIGGER; then
-  cat > /etc/systemd/system/node_exporter.service <<EOM
-[Unit]
-Description=Node Exporter
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=node_exporter
-Group=node_exporter
-Type=simple
-ExecStart=/usr/local/bin/node_exporter \
-  --collector.systemd \
-  --collector.processes \
-  --web.listen-address=:9500
-
-[Install]
-WantedBy=multi-user.target
-EOM
-
-  systemctl daemon-reload
-  systemctl enable node_exporter
-  systemctl start node_exporter
+  init_node_exporter
 fi
 
-systemctl status node_exporter | awk 'NR==3 {printf "\nStatus of node_exporter: %s\n", $2}'
+set_prometheus_folders
 
 if $UPDATE_PROMETHEUS && $PROMETHEUS_TRIGGER; then
-  printf "\nUpdate Prometheus\n"
-  test -f prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}.tar.gz || wget https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}.tar.gz
-
-  tar xfz prometheus-*.tar.gz &> /dev/null
-  cp prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/prometheus /usr/local/bin
-  cp prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/promtool /usr/local/bin
-  chown prometheus:prometheus /usr/local/bin/prometheus
-  chown prometheus:prometheus /usr/local/bin/promtool
-
-  cp -r prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/consoles /etc/prometheus
-  cp -r prometheus-${PROMETHEUS_VERSION}.linux-${SYSTEM_ARCH}/console_libraries /etc/prometheus
-  chown -R prometheus:prometheus /etc/prometheus/consoles
-  chown -R prometheus:prometheus /etc/prometheus/console_libraries
-
-  rm -rf prometheus-${PROMETHEUS_VERSION}*
+  update_prometheus
 fi
 
 if $INIT_PROMETHEUS && $PROMETHEUS_TRIGGER; then
-  cat > /etc/prometheus/prometheus.yml <<EOM
-global:
-  scrape_interval:     15s
-  evaluation_interval: 15s
-
-rule_files:
-  # - "first.rules"
-  # - "second.rules"
-
-scrape_configs:
-  - job_name: 'prometheus'
-    scrape_interval: 5s
-    static_configs:
-      - targets: ['localhost:9590']
-
-  - job_name: 'node_exporter'
-    scrape_interval: 5s
-    static_configs:
-      - targets: ['localhost:9500']
-EOM
-
-cat > /etc/systemd/system/prometheus.service <<EOM
-[Unit]
-  Description=Prometheus Monitoring
-  Wants=network-online.target
-  After=network-online.target
-
-[Service]
-  User=prometheus
-  Group=prometheus
-  Type=simple
-  ExecStart=/usr/local/bin/prometheus \
-  --config.file /etc/prometheus/prometheus.yml \
-  --storage.tsdb.path /var/lib/prometheus/ \
-  --web.console.templates=/etc/prometheus/consoles \
-  --web.console.libraries=/etc/prometheus/console_libraries \
-  --web.listen-address=:9590
-  ExecReload=/bin/kill -HUP $MAINPID
-
-[Install]
-  WantedBy=multi-user.target
-EOM
-
-  systemctl daemon-reload
-  systemctl enable prometheus
-  systemctl start prometheus
+  init_prometheus
 fi
 
-systemctl status prometheus | awk 'NR==3 {printf "\nStatus of Prometheus: %s\n", $2}'
+get_status
