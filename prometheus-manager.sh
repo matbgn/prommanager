9f534fdf774c8aabc66143d882abfe156b602da1
Matthieu Borgognon <matthieu.borgognon@cse.ch>
2022-02-01 10:11:30 +0100

    Update versions for both node_exporter and prometheus

--- prometheus-manager.sh
+++ prometheus-manager.sh
@@ -4,8 +4,8 @@ printf "Welcome to Prometheus manager v0.1.1!\n\n" # print to screen
 # Set default values
 SYSTEM_ARCH=amd64 # -> can be changed by script argument -a arm64
 
-NODE_EXPORTER_VERSION=1.3.0 # -> can be changed by script argument -N 1.1.2
-PROMETHEUS_VERSION=2.31.1 # -> can be changed by script argument -P 2.27.1
+NODE_EXPORTER_VERSION=1.3.1 # -> can be changed by script argument -N 1.1.2
+PROMETHEUS_VERSION=2.33.0 # -> can be changed by script argument -P 2.27.1
 
 NODE_EXPORTER_PORT=9500
 PROMETHEUS_PORT=9590
